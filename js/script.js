7248ba1bf340a9f1b616e2cf0f49e6d0c92e4b8e
djevy <60827567+djevy@users.noreply.github.com>
2021-01-19 15:25:49 +0000

    Initial Setup
    Completed the initial project setup and file structure.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+$(window).on('load', function () {
+    if ($('#preloader').length) {
+    $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
+    });
+    }
+});
