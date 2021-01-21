f2e10bc2c350b19a3ee52194e4fe11d94797bc0d
djevy <60827567+djevy@users.noreply.github.com>
2021-01-21 16:58:12 +0000

    Update script.js
    Added base ajax call to php file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 
-
 $(window).on('load', function () {
     if ($('#preloader').length) {
     $('#preloader').delay(100).fadeOut('slow', function () {
@@ -7,3 +6,89 @@ $(window).on('load', function () {
     });
     }
 });
+
+$('#btn1').click(function() {
+
+    $.ajax({
+        url: "php/getTimezoneInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            
+        },
+        success: function(result) {
+
+            console.log(result);
+
+            if (result.status.name == "ok") {
+
+                $('#').html(result['data'][0]['']);
+                
+
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("There has been an error!")
+        }
+    }); 
+
+});
+
+$('#btn2').click(function() {
+
+    $.ajax({
+        url: "php/getEarthquakeInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            
+        },
+        success: function(result) {
+
+            console.log(result);
+
+            if (result.status.name == "ok") {
+
+                $('#').html(result['data'][0]['']);
+                
+
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("There has been an error!")
+        }
+    }); 
+
+});
+
+$('#btn3').click(function() {
+
+    $.ajax({
+        url: "php/getWeatherInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            
+        },
+        success: function(result) {
+
+            console.log(result);
+
+            if (result.status.name == "ok") {
+
+                $('#').html(result['data'][0]['']);
+                
+
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("There has been an error!")
+        }
+    }); 
+
+});
+
+
